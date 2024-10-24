04391aaf6ba21906005a5908a42520986e47c4dc
Clancy-dev <132342072+Clancy-dev@users.noreply.github.com>
2024-10-24 18:51:22 +0300

    the

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <h1>Github-developer-finder</h1>
+        <h2>Under Developer</h2>
+      
+    </div>
+  )
+}
+         
